8b7a01d5c4c6b2b969de2d9537f4c972d8f2a508
samolukadjo <82143316+samolukadjo@users.noreply.github.com>
2024-01-23 13:52:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const title = document.getElementById("title");
 function changeBar(i) {
     let num = i;
     setTimeout(function () {
-        if (num <= 100) {
+        if (num <= 1000) {
             bar.setAttribute("value", num);
-            console.log(num * 20 + "ms");
+            console.log(num * 2 + "ms");
             num++;
             changeBar(num);
         } else {
             title.textContent = "Success!";
         }
-    }, num * 20);
+    }, num * 2);
 }
 
 changeBar(0);
